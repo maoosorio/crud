688d1337eb9e7cfa4f5ff16931546886b8df5e77
Mario Alberto Solórzano Osorio <isc_osorio@hotmail.com>
2021-08-12 14:52:34 -0500

    Login y Estilos
    Agregamos auth y bootstrap para el crud

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\EmpleadoController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
+});
+
+// Route::get('/empleado', function () {
+//     return view('empleado.index');
+// });
+
+// Route::get('/empleado/create', [EmpleadoController::class,'create']);
+
+Route::resource('empleado', EmpleadoController::class)->middleware('auth');
+Auth::routes(['register'=>false,'reset'=>false]); //lo que esta entre parentesis es lo que quieres que no aparezca
+
+Route::get('/home', [EmpleadoController::class, 'index'])->name('home');
+
+Route::group(['middleware' => 'auth'], function(){
+    Route::get('/', [EmpleadoController::class, 'index'])->name('home');
 });
